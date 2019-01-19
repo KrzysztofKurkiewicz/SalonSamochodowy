c78c9a8499ddbf352e53f3b5f14a1b93751327d3
KrzysztofKurkiewicz <krzysztof.kurkiewicz@softmaks.pl>
2019-01-19 12:01:37 +0100

    test

--- SalonSamochodowy/Wyposazenie.cs
+++ SalonSamochodowy/Wyposazenie.cs
@@ -6,11 +6,14 @@ using System.Threading.Tasks;
 
 namespace SalonSamochodowy
 {
+    
     public class Wyposazenie
     {
         public string Nazwa { get; set; }
         public int Cena { get; set; }
         public bool CzyStandard { get; set; }
+
+        private int testZmiennej;
     }
 
 }
